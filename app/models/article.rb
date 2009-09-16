0898be8e4cc4108ba7b9265d5ebc04bae2c3b70b
Woody <woody1983@vip.qq.com>
2009-09-16 14:06:28 +0800

    RSS is finsh.

---
+++ app/models/article.rb
@@ -0,0 +1,22 @@
+class Article < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :category
+	validates_presence_of :title
+	validates_presence_of :synopsis
+	validates_presence_of :body
+	validates_length_of :title, :maximum => 255
+	validates_length_of :synopsis, :maximum => 1000
+	validates_length_of :body, :maximum => 20000
+	
+	
+	cattr_reader :per_page 
+    @@per_page = 5 
+	
+	before_save :update_published_at
+	
+	def update_published_at
+		self.published_at = Time.now if published == true
+	end
+	
+	
+end
---
+++ lib/login_system.rb
@@ -0,0 +1,61 @@
+module LoginSystem
+	protected
+	
+	def is_logged_in?
+		@logged_in_user = User.find(session[:user]) if session[:user]
+	end
+	
+	def logged_in_user
+		return @logged_in_user if is_logged_in?
+	end
+	
+	def logged_in_user=(user)
+		if !user.nil?
+			session[:user] = user.id
+			@logged_in_user = user
+		end
+	end
+	
+	def self.included(base)
+		base.send :helper_method, :is_logged_in?, :logged_in_user
+	end
+	
+	def check_role(role)
+		unless is_logged_in? && @logged_in_user.has_role?(role)
+			flash[:error] = "You do not have the permission to do that."
+			redirect_to :controller => 'account', :action => 'login'
+		end
+	end
+	
+	def check_administarator_role
+		check_role('Administrator')
+	end
+	
+	def login_required
+		unless is_logged_in?
+			flash[:error] = "You must be logged in to do that."
+			redirect_to :controller => 'account', :action => 'login'
+		end
+	end
+	
+	def check_editor_role
+		check_role('Editor')
+	end
+	
+	#----------为API添加HTTP验证－－－－－－－－＃
+	
+	private
+	def get_http_auth_data
+		username, password = nil, nil
+		auth_headers = [ 'X-HTTP_AUTHORIZATION','Authorization', 'HTTP_AUTHORIZATION',
+						'REDIRECT_REDIRECT_X_http_AUTHORIZATION']
+		auth_header = auth_header.detect { |key| request.env|key| }
+		auth_data = request.env[auth_header].to_s.split
+		
+		if auth_data && auth_data[0] = 'Basic'
+			username, password = Base64.decode64(auth_data[1]).split(':')[0..1]
+		end
+		return [username, password]
+	end
+	
+end
